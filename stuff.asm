f3e92c88623d478be44dcae45448ab2c46481f5b
SecretShop <shot2400@gmail.com>
2016-04-14 17:25:07 -0500

    Connected Loop to increment
    Also cleaned up all of the code

--- stuff.asm
+++ stuff.asm
@@ -3,22 +3,13 @@
 cseg at 0
 	mov P2.0, #0
 
-loop:
-	mov c, P2.0 
-	jnc led_on
-	sjmp loop
-
-led_on:
-	mov P2.4, #0x01
-	sjmp loop
+WAIT_INPUT:							
+	jb P2.0 INCREMENT			;Check for Increment button, Sw1
+	jb P0.1 DECREMENT			;Check for Decrement button, Sw2
+	sjmp wait_input				;will continue to loop here until Sw1 is pressed
 
 
-
-
-;fucntions:
-
-;100ms delay
-DELAY:
+DELAY:							;100ms delay for input
 	MOV R0 0x91
 DLOOP:	MOV R1 0xFF
 DLOOP2: DJNZ R1, DLOOP2
@@ -26,13 +17,9 @@ DLOOP2: DJNZ R1, DLOOP2
 	RET
 
 
-
-;iNCREMENT AND dECREMENT STUFFS
-;If Inc button pressed (Sw1)
-;0=ON   1=OFF
-inc button pressed
-	setb c ;set carry to 1
-L1:
+INCREMENT:						;Will increment by 1 0=ON and 1=OFF			
+	setb c 						;set carry to 1 which will be used to increment
+L1:								;All of these loops are used to increment
 	JC LED1
 L2:
 	JC LED2
@@ -40,55 +27,52 @@ L3:
 	JC LED3
 L4:
 	JC LED4
-Done:
-	sjmp (CHECKING FOR INPUT)
-
-;Here are all the actions for the LED's
+DONE:
+	sjmp DELAY 					;jumps up to delay input
 LED1: 
 	JB p2.4, LED1_ISOFF
 	JNB p2.4 LED1_ISON
 LED1_ISON:
-	setb p2.4 	;Turning LED off
+	setb p2.4 					;Turning LED off
 LED1_ISOFF:
-	clr p2.4 	;Turning LED on
-	clr c		;Clear C
+	clr p2.4 					;Turning LED on
+	clr c					
 	sjmp L2
 ;LED 2
 LED2: 
 	JB p0.5, LED2_ISOFF
 	JNB p0.5 LED2_ISON
 LED2_ISON:
-	setb p0.5 	;Turning LED off
+	setb p0.5 					;Turning LED off
 LED2_ISOFF:
-	clr p0.5 	;Turning LED on
-	clr c		;Clear C
+	clr p0.5 					;Turning LED on
+	clr c						
 	sjmp L3
 ;LED 3
 LED3: 
 	JB p2.7, LED3_ISOFF
 	JNB p2.7 LED3_ISON
 LED3_ISON:
-	setb p2.7 	;Turning LED off
+	setb p2.7 					;Turning LED off
 LED3_ISOFF:
-	clr p2.7 	;Turning LED on
-	clr c		;Clear C
+	clr p2.7 					;Turning LED on
+	clr c		
 	sjmp L4
 ;LED 4
 LED4: 
 	JB p0.6, LED4_ISOFF
 	JNB p0.6 LED4_ISON
 LED4_ISON:
-	setb p0.6 	;Turning LED off
-	;Clears all the LED's because over 15
-	setb p2.4
+	setb p0.6 					;Clears all the LED's because over 15
+	setb p2.4					
 	setb p0.5
 	setb p2.7
 	setb p0.6
 
 LED4_ISOFF:
-	clr p0.6 	;Turning LED on
-	clr c		;Clear C
-	sjmp Done
+	clr p0.6 					;Turning LED on
+	clr c		
+	sjmp DONE
 
 
 
